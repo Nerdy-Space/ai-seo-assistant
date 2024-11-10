6e1189d7c3127821550e23bc78a66b0a06a5343c
AfnanFerdousi <afnanferdousi550@gmail.com>
2024-11-10 18:23:24 +0600

    [DONE] ogGenerator done

---
+++ src/app/open-graph-generator/page.tsx
@@ -0,0 +1,12 @@
+import GraphGenerator from '@/components/pages/ogGenerator/Generate';
+import React from 'react';
+
+const OpenGraphGenerator = () => {
+    return (
+        <div>
+            <GraphGenerator/>
+        </div>
+    );
+};
+
+export default OpenGraphGenerator;
